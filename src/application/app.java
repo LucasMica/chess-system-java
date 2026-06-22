1a6a93e7337a353193252fbffa4baf4d52471364
Lucas <lucasmicaoliveira@gmail.com>
2026-06-22 11:18:27 -0300

    class Position

--- src/application/app.java
+++ src/application/app.java
@@ -1,7 +1,13 @@
 package application;
 
+import boardgame.Position;
+
+import java.util.Arrays;
+
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position p = new Position(1,2);
+
+        System.out.println(p);
     }
 }
