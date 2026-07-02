2995ae09075df0f50a01a3c01d82725095d57171
Lucas <lucasmicaoliveira@gmail.com>
2026-07-02 13:06:57 -0300

    Moving pieces

--- src/application/app.java
+++ src/application/app.java
@@ -2,13 +2,30 @@ package application;
 
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class app {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch ch = new ChessMatch();
-        UI.printBoard(ch.getPieces());
+
+        while(true) {
+            UI.printBoard(ch.getPieces());
+            System.out.println();
+            System.out.println("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+
+            ChessPiece capturedPiece = ch.performChessMove(source, target);
+        }
     }
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -32,7 +32,7 @@ public class Board {
 
     public Piece piece(Position position) {
         if (!positionExists(position)) {
-            throw new BoardException("Position not in the board");
+            throw new BoardException("Position is not in the board");
         }
         return pieces[position.getRow()][position.getColumn()];
     }
@@ -43,6 +43,22 @@ public class Board {
         }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    public Piece removePiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position is not in the board");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+
+        aux.position = null;
+
+        pieces[position.getRow()][position.getColumn()] = null;
+
+       return aux;
     }
 
     private boolean positionExists(int row, int column) {
@@ -57,4 +73,5 @@ public class Board {
         positionExists(position);
         return piece(position) != null;
     }
+
 }
